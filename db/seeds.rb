49b8efc99dbd0c7b07f4b222520019194ebcf058
danielle levy <danielle.levyy@gmail.com>
2020-03-31 19:47:09 +0100

    pgsearch gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,28 @@
 # # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 # require 'faker'
 # # Examples:
-# #
+require 'pg_search'
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
-# puts 'Creating 15 fake items...'
-# 15.times do
-#   item = Item.new(
-#     name:    Faker::Food.ingredient,
-#     category: "Canned",
-#     purchased: Time.now,
-#     expires:  Time.now + 365.days,
-#     user_id: 1
-#   )
-#   item.save!
-# end
-# puts 'Finished!'
+puts 'Creating 35 fake items...'
+5.times do
+  u = User.create!(
+    email: Faker::Internet.free_email,
+    password: "123456",
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.name
+  )
+end
+
+30.times do
+  item = Item.new(
+    name:    Faker::Food.ingredient,
+    category: "Fruit",
+    purchased: Time.now,
+    expires:  Time.now + 365.days,
+    user_id: rand(1..5)
+  )
+  item.save!
+end
+
+puts 'Finished!'
