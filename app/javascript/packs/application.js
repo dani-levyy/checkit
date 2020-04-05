86f56ef535833faa71719ab1bc623a54ceea49ec
danielle levy <danielle.levyy@gmail.com>
2020-04-05 18:36:17 +0100

    working on chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("chartkick")
 require("chart.js")
 
 //= require chartkick
-//= require Chart.bundle
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -35,4 +35,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+    // const chartkick = document.querySelector("chart.js")
 });
